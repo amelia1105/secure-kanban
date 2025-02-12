c5e240c22f1236a40d235ec00bc21ec7d20dfbee
Amelia <aebellanger@yahoo.com>
2025-02-12 17:54:58 -0500

    Complete authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,31 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try {
+    // send POST request to /auth/login
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    });
+
+    // throw error if response not OK
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  
+  } catch (err) {
+    // log and return errors
+    console.log('Error from user login: ', err);
+    return Promise.reject('Could not fetch user info');
+  }
 }
-
-
 
 export { login };
