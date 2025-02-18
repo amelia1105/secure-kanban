2221eeed6b8bd1af3e13bccbbb65ca389483f983
Amelia <aebellanger@yahoo.com>
2025-02-17 21:40:50 -0500

    Update middleware

--- client/src/api/userAPI.tsx
+++ client/src/api/userAPI.tsx
@@ -2,22 +2,28 @@ import Auth from '../utils/auth';
 
 const retrieveUsers = async () => {
   try {
+    const token = Auth.getToken();  // Get token from AuthService
+    if (!token) {
+      throw new Error('No token available');
+    }
+
     const response = await fetch('/api/users', {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${Auth.getToken()}`
       }
-    });
-    const data = await response.json();
+    });    
 
-    if(!response.ok) {
-      throw new Error('invalid user API response, check network tab!');
+    if (!response.ok) {
+      throw new Error('Failed to retrieve users');
     }
 
+    const data = await response.json();
     return data;
 
-  } catch (err) { 
-    console.log('Error from data retrieval:', err);
+  } catch (err) {
+    console.error('Error from data retrieval:', err);
     return [];
   }
 }
